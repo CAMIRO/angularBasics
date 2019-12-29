9a33b68fc393124822668a68745a2de69bf28902
Juan Sarmiento <juansarmiento@Mac-mini-de-TeraAdmin-2.local>
2019-12-29 08:48:24 -0500

    working with HTML displaying and Outupping Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-recipe-instant-pot-meatballs%2FInstant-Pot-Meatballs_006'),
     new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-recipe-instant-pot-meatballs%2FInstant-Pot-Meatballs_006')
   ];
 
